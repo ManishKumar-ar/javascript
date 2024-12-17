cfb416de876f73d6b3a7fff3ddb28d11e9c59438
Manish kumar <134220511+ManishKumar-ar@users.noreply.github.com>
2024-12-17 17:41:01 +0530

    commit

---
+++ section - 6(Function)/002_functionAdavance.js
@@ -0,0 +1,9 @@
+const greet = function greetFunction() {
+    console.log("Hello, World!");
+};
+
+
+// Here, the function has a name greetFunction, 
+// but you still call it using the variable name greet.
+// The name greetFunction is not needed,
+// unless you want to refer to the function inside itself (like for recursion or debugging).

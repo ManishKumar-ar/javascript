de76994d8a7ff87195f6c84c4b8f2159489c96a4
Manish kumar <134220511+ManishKumar-ar@users.noreply.github.com>
2025-01-18 10:57:54 +0530

    Add files via upload

---
+++ section - 05(object)/018_Exercise_4_BlogPostObject.js
@@ -0,0 +1,23 @@
+//create a post object that have - 
+
+
+
+const post = {
+    title: "coder",
+    body: "this is the content of the post",
+    author: "manish kumar",
+    views: 1000,
+    Comment: [{
+        author: "ankkit",
+        body: "random"
+    }, {
+        author2: "ankki2t",
+        body2: "random2"
+
+    }
+    ],
+    isLive: true
+
+}
+
+console.log(post);

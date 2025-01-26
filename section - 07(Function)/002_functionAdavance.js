e60189fe6e0b74dc648e54ea42d352d6ca9ab82d
Manish kumar <134220511+ManishKumar-ar@users.noreply.github.com>
2025-01-26 07:44:32 +0530

    delete

--- section - 07(Function)/002_functionAdavance.js
+++
@@ -1,9 +0,0 @@
-const greet = function greetFunction() {
-    console.log("Hello, World!");
-};
-
-
-// Here, the function has a name greetFunction, 
-// but you still call it using the variable name greet.
-// The name greetFunction is not needed,
-// unless you want to refer to the function inside itself (like for recursion or debugging).
--- section - 07(Function)/003_functionAdavance.js
+++
@@ -1,7 +0,0 @@
-const greet = function greetFunction() {
-    console.log("Hello, World!");
-    greetFunction();           // This is valid because greetFunction is visible inside its own scope
-};
-
-greet();
-// This will call itself recursively, but without a base case, it will cause a stack overflow...
